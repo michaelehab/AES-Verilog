ebb98b922c09276302511dd975aacff3d60c1d74
ebramatef00 <ebraamatef@outlook.com>
2022-05-03 02:59:45 +0200

    rcon and addroundkey

--- addRoundKey.v
+++ addRoundKey.v
@@ -1,2 +1,10 @@
-module addRoundKey;
+module addRoundKey(input [127 : 0] data, output [127:0] out, input [127:0] key, input clk);
+
+generate
+genvar i;
+for(i = 0; i < 128; i = i + 1)
+begin: h
+assign out[i] = data[i]^key[i];
+end
+endgenerate
 endmodule
