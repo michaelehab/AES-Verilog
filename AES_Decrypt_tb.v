98839e0b544edf2994f3d8b4eebabce09bb3fa8b
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2022-05-09 11:04:33 +0200

    AES decrypt tested with 128, 192 and 256-bit

--- AES_Decrypt_tb.v
+++ AES_Decrypt_tb.v
@@ -12,8 +12,8 @@ wire [127:0] out3;
 reg [255:0] key3;
 
 AES_Decrypt a(in1,key1,out1);
-//AES_Encrypt #(192,12,6) b(in2,key2,out2);
-//AES_Encrypt #(256,14,8) c(in3,key3,out3);
+AES_Decrypt #(192,12,6) b(in2,key2,out2);
+AES_Decrypt #(256,14,8) c(in3,key3,out3);
 
 
 initial begin
@@ -21,14 +21,14 @@ $monitor("in128= %h, key128= %h ,out128= %h",in1,key1,out1);
 in1=128'h69c4e0d86a7b0430d8cdb78070b4c55a;
 key1=128'h000102030405060708090a0b0c0d0e0f;
 #10;
-//$monitor("in192= %h, key192= %h ,out192= %h",in2,key2,out2);
-//in2=128'h_00112233_44556677_8899aabb_ccddeeff;
-//key2=192'h_00010203_04050607_08090a0b_0c0d0e0f_10111213_14151617;
-//#10;
-//$monitor("in256= %h, key256= %h ,out256= %h",in3,key3,out3);
-//in3=128'h_00112233_44556677_8899aabb_ccddeeff;
-//key3=256'h_00010203_04050607_08090a0b_0c0d0e0f_10111213_14151617_18191a1b_1c1d1e1f;
-//#10;
+$monitor("in192= %h, key192= %h ,out192= %h",in2,key2,out2);
+in2=128'hdda97ca4864cdfe06eaf70a0ec0d7191;
+key2=192'h000102030405060708090a0b0c0d0e0f1011121314151617;
+#10;
+$monitor("in256= %h, key256= %h ,out256= %h",in3,key3,out3);
+in3=128'h8ea2b7ca516745bfeafc49904b496089;
+key3=256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+#10;
 end
 
 endmodule
