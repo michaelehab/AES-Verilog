aedf4b3bfb7d14b7800a24116bbc5a6b07ea4d39
Michael Ehab <michaelehab16@gmail.com>
2022-05-22 13:27:46 +0200

    Modified the Wrapper module to test all the cases individually

--- AES.v
+++ AES.v
@@ -1,5 +1,11 @@
-module AES(enable, led);
-output wire led;
+module AES(enable, e128, d128, e192, d192, e256, d256);
+
+output wire e128;
+output wire d128;
+output wire e192;
+output wire d192;
+output wire e256;
+output wire d256;
 input enable;
 
 // The plain text used as input
@@ -10,10 +16,19 @@ wire[127:0] key128 = 128'h_000102030405060708090a0b0c0d0e0f;
 wire[191:0] key192 = 192'h_000102030405060708090a0b0c0d0e0f1011121314151617;
 wire[255:0] key256 = 256'h_000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
 
+// The expected outputs from the encryption module
+wire[127:0] expected128 = 128'h_69c4e0d86a7b0430d8cdb78070b4c55a;
+wire[127:0] expected192 = 128'h_dda97ca4864cdfe06eaf70a0ec0d7191;
+wire[127:0] expected256 = 128'h_8ea2b7ca516745bfeafc49904b496089;
+
 // The result of the encryption module for every type
 wire[127:0] encrypted128;
 wire[127:0] encrypted192;
 wire[127:0] encrypted256;
+
+assign e128 = (encrypted128 == expected128 && enable) ? 1'b1 : 1'b0;
+assign e192 = (encrypted192 == expected192 && enable) ? 1'b1 : 1'b0;
+assign e256 = (encrypted256 == expected256 && enable) ? 1'b1 : 1'b0;
 
 // The result of the decryption module for every type
 wire[127:0] decrypted128;
@@ -28,7 +43,8 @@ AES_Decrypt a2(encrypted128,key128,decrypted128);
 AES_Decrypt #(192,12,6) b2(encrypted192,key192,decrypted192);
 AES_Decrypt #(256,14,8) c2(encrypted256,key256,decrypted256);
 
-// The led will be on only if the decryption result is the same as the plain text
-assign led = (decrypted128 == in && decrypted192 == in && decrypted256 == in && enable) ? 1'b1 : 1'b0;
+assign d128 = (decrypted128 == in && enable) ? 1'b1 : 1'b0;
+assign d192 = (decrypted192 == in && enable) ? 1'b1 : 1'b0;
+assign d256 = (decrypted256 == in && enable) ? 1'b1 : 1'b0;
 
 endmodule
--- AES_tb.v
+++ AES_tb.v
@@ -1,21 +1,27 @@
 module AES_tb;
 
-wire led;
+wire e128;
+wire d128;
+wire e192;
+wire d192;
+wire e256;
+wire d256;
 reg enable;
 
-AES a(enable, led);
+AES a(enable, e128, d128, e192, d192, e256, d256);
 
 initial begin
-	$monitor("Led = %b", led);
-	// Setting the initial values
+	$monitor("Encrypt128 = %b, Decrypt128 = %b, Encrypt192 = %b, Decrypt192 = %b, Encrypt256 = %b, Decrypt256 = %b",
+		e128, d128, e192, d192, e256, d256);
+	// Turning on enable to check that all tests passed
 	enable = 1;
 	#10;
 	
-	// Turning off reset to check if the led turns on
+	// Turning off enable to check if the leds turn off
 	enable = 0;
 	#10;
 	
-	// Turning on reset to check if the led turns off
+	// Turning on enable to check if the leds turn on again
 	enable = 1;
 	#10;
 end
