6be0a0d2d223855f2fbfdd5765bf4dafcffc3442
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2022-05-24 17:43:18 +0200

    Added comments to MixColumns and InverseMixColumns

--- inverseMixColumns.v
+++ inverseMixColumns.v
@@ -2,6 +2,13 @@ module inverseMixColumns(state_in,state_out);
 input [127:0] state_in;
 output [127:0] state_out;
 
+/*
+Multiplication by 2 can be implemented at the byte level as a left shift and a subsequent
+conditional bitwise XOR with {1b},where bitwise xor with {1b} is done if x[7]= 1.
+Multiplication by higher powers of x can be implemented by repeated application of multiplication by 2.
+*/
+
+//This function multiply by {02} n-times
 function[7:0] multiply(input [7:0]x,input integer n);
 integer i;
 begin
@@ -14,15 +21,29 @@ end
 
 endfunction
 
-function [7:0] mb0e; 
+
+/* 
+	Multiply by {0e} is done by :
+	(multiplying by {02} 3 times which is equivalent to multiplication by {08}) xor
+	(multiplying by {02} 2 times which is equivalent to multiplication by {04}) xor
+	(multiplying by {02})
+	so that 8+4+2= e. where xor is the addition of elements in finite fields
+*/
+function [7:0] mb0e; //multiply by {0e}
 input [7:0] x;
 begin
 	mb0e=multiply(x,3) ^ multiply(x,2)^ multiply(x,1);
 end
 endfunction
 
-
-function [7:0] mb0d; 
+/* 
+	Multiply by {0d} is done by :
+	(multiplying by {02} 3 times which is equivalent to multiplication by {08}) xor
+	(multiplying by {02} 2 times which is equivalent to multiplication by {04}) xor
+	(the original x)
+	so that 8+4+1= d. where xor is the addition of elements in finite fields
+*/
+function [7:0] mb0d; //multiply by {0d}
 input [7:0] x;
 begin
 	mb0d=multiply(x,3) ^ multiply(x,2)^ x;
@@ -30,14 +51,26 @@ end
 endfunction
 
 
-function [7:0] mb0b; 
+/* 
+	Multiply by {0b} is done by :
+	(multiplying by {02} 3 times which is equivalent to multiplication by {08}) xor
+	(multiplying by {02}) xor (the original x)
+	so that 8+2+1= b. where xor is the addition of elements in finite fields
+*/
+
+function [7:0] mb0b;  //multiply by {0b}
 input [7:0] x;
 begin
 	mb0b=multiply(x,3) ^ multiply(x,1)^ x;
 end
 endfunction
+/* 
+	Multiply by {09} is done by :
+	(multiplying by {02} 3 times which is equivalent to multiplication by {08}) xor (the original x)
+	so that 8+1= 9. where xor is the addition of elements in finite fields
+*/
 
-function [7:0] mb09; 
+function [7:0] mb09; //multiply by {09}
 input [7:0] x;
 begin
 	mb09=multiply(x,3) ^  x;
--- mixColumns.v
+++ mixColumns.v
@@ -7,18 +7,23 @@ output[127:0] state_out;
 function [7:0] mb2; //multiply by 2
 	input [7:0] x;
 	begin 
-			/* multiplication by 2 is shifting on bit to the left, and if the original 8 bits had a 1 @ MSB, xor the result with 0001 1011*/
+			/* multiplication by 2 is shifting on bit to the left, and if the original 8 bits had a 1 @ MSB,
+			xor the result with {1b}*/
 			if(x[7] == 1) mb2 = ((x << 1) ^ 8'h1b);
 			else mb2 = x << 1; 
 	end 	
 endfunction
 
 
-
+/* 
+	multiplication by 3 is done by:
+		multiplication by {02} xor(the original x)
+		so that 2+1=3. where xor is the addition of elements in finite fields
+*/
 function [7:0] mb3; //multiply by 3
 	input [7:0] x;
 	begin 
-			/* multiplication by 3 ,= 01 ^ 10 = (NUM * 01) XOR (NUM * 10) = (NUM) XOR (NUM Muliplication by 2) */
+			
 			mb3 = mb2(x) ^ x;
 	end 
 endfunction
